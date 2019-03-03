841bfbd5f57a3d227bf77828f89af3a1578fe24d
Misha <mishakryvoshea@gmail.com>
2019-03-03 21:06:13 +0200

    first commit

---
+++ src/Practice3.java
@@ -0,0 +1,15 @@
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5;
+		int b=67;
+		for(int i=0;i<b;i++){
+			a++;
+		}
+	System.out.print(a);
+	}
+
+}
